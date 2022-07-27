80d2dd986d22cb56d84c7e32de9511e71370560c
VladlmlrF <vladimirfalin84@gmail.com>
2022-07-27 19:09:20 +0300

    created start screen

---
+++ SpaceXApp/ViewControllers/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  SpaceXApp
+//
+//  Created by Владимир Фалин on 27.07.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    private var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        timer = Timer.scheduledTimer(
+            timeInterval: 3,
+            target: self,
+            selector: #selector(someFunc),
+            userInfo: nil,
+            repeats: false
+        )
+    }
+
+    @objc func someFunc() {
+        performSegue(withIdentifier: "toMainScreen", sender: nil)
+    }
+}
