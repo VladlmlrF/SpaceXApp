4e0855857bcf1f1781654078d4bff7bb0a869afa
VladlmlrF <vladimirfalin84@gmail.com>
2022-07-30 17:42:44 +0300

    create new branch

---
+++ SpaceXApp/Models/Settings.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  SpaceXApp
+//
+//  Created by Anastasia on 28.07.22.
+//
+
+import Foundation
+
+struct Settings {
+    enum LengthUnit: String {
+        case m
+        case ft
+    }
+    
+    enum MassUnit: String {
+        case kg
+        case ib = "Ib"
+    }
+
+    var height: LengthUnit
+    var diameter: LengthUnit
+    
+    var weight: MassUnit
+    var payload: MassUnit
+    
+}
